a93060b295303ebf2bd996f8d1e1ecd2de02b759
Christian Hesels <christian.hesels@fokus.fraunhofer.de>
2024-04-21 08:36:28 +0200

    remove unused variables

--- variables.tf
+++ variables.tf
@@ -5,15 +5,6 @@ variable "aws_region" {
   default = "eu-central-1"
 }
 
-variable "vpc_cidr_a" {
-  default = "10.1.0.0/16"
-}
-
-
-variable "vpc_cidr_b" {
-  default = "10.2.0.0/16"
-}
-
 variable "domain_name" {
   default = "your.domain.de"
 }
