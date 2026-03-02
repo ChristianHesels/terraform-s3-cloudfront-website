fc0a9a9cdf4992f0bfc46894c62844eb4f5bbf2a
Christian Hesels <christianhesels@gmail.com>
2026-03-02 13:33:37 +0100

    Add wildcard subdomain to ACM certificate

--- acm-cert/main.tf
+++ acm-cert/main.tf
@@ -8,6 +8,7 @@ terraform {
 
 resource "aws_acm_certificate" "acm_cert" {
   domain_name       = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
   validation_method = "DNS"
   lifecycle {
     create_before_destroy = true
